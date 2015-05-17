c025091c102ea161deb2b87f329af76df7696068
Marcus Stewart Hughes <hello@entirely.pro>
2015-05-17 19:52:51 +0100

    if PAUSELOCK!

--- globalcombined.lua
+++ globalcombined.lua
@@ -62,7 +62,7 @@ function et_ClientCommand(cno,cmd)
 		end
 	end
 
-	if cmd == "pause" and tonumber(et.trap_Cvar_Get("sv_maxclients")) == 1 then
+	if cmd == "pause" and tonumber(et.trap_Cvar_Get("pauselock")) == 1 then
 		et.trap_SendServerCommand( cno , "print \"Pause captured.\n\"" )
 		return 1
 	end
