86213e6db9c6b7a75e617d7bf0a2414b2715cc4b
Marcus Hughes <hello@msh100.uk>
2017-03-10 19:58:54 +0000

    only do it on commits to master

--- redeploy.sh
+++ redeploy.sh
@@ -1,4 +1,4 @@
-if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "test" ]; then
+if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
     wget https://gist.githubusercontent.com/nouseforname/ca53692d8d6d29929b82/raw/85a983117aace77aef23fcb81540aa84f7aec3a7/rcon.sh
     chmod +x rcon.sh
     ./rcon.sh -host=config.msh100.uk:27960 -pw=${RCONPASS} "quit"
